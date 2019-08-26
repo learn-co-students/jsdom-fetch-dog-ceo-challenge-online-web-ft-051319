57156334cd2bfa3f0fa8319661d88f82a0e35726
Natalie <natsetc@gmail.com>
2019-08-26 02:25:00 -0700

    notes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ console.log('%c HI', 'color: firebrick')
 document.addEventListener('DOMContentLoaded', function() {
     fetchImages()
     fetchBreeds()
+    filterBreeds()
 })
 
 function fetchImages() {
@@ -40,15 +41,22 @@ function renderBreeds(json) {
 
 function filterBreeds() {
     const breedDropdown = document.querySelector('#breed-dropdown');
-    const letter = breedDropdown.value;
-    const ul = document.querySelector('#dog-breeds');
-    const li = ul.getElementsByTagName('li');
+    
+    // li[i].innerText.charAt(0) === letter
+    // other way to compare first letter of breed to filter letter
 
-    for (i = 0; i < li.length; i++) {
-        if (li[i].innerText.charAt(0) === letter) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+    breedDropdown.addEventListener('change', () => {
+        const letter = breedDropdown.value;
+        const ul = document.querySelector('#dog-breeds');
+        const li = ul.getElementsByTagName('li');
+        for (i = 0; i < li.length; i++) {
+            if (li[i].innerText.startsWith(letter)) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
         }
-    }
+    })
+
+    // if onchange="filterBreeds()" not added to html select tag, use above event listener on breedDropdown and add filterBreeds() to functions to execute when DOMContentLoaded
 };
